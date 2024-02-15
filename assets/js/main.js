9388e0277481e3475c9fd46ba577627b02e3078a
Leonardo Coelho <leonardo.coelho@experienceone.com>
2024-02-15 16:33:17 +0100

    fetching penas

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,12 @@
-console.log("teste")
+document.addEventListener('DOMContentLoaded', async () => { 
+  const penasPath = 'assets/data/penas.json';
+
+  const getData = async (path) => {
+    const response = await fetch(path);
+    const data = await response.json();
+    return data;
+  }
+
+  const penas = await getData(penasPath);
+  console.log(penas);
+});
